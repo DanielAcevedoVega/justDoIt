a8ce7faa6431450e9435610cd9ac48a13293c637
DanielAcevedoVega <antonydany236@gmail.com>
2024-06-25 19:05:33 -0500

    feat: :sparkles: Se creo una funcion para agregar una task a la base de datos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 import { indexTask } from "./components/taskList.js";
-import { allTaskList } from "./module/app.js";
+import { addTask, allTaskList } from "./module/app.js";
 
 let listTask = document.querySelector('.list__task');
+let btnAdd = document.querySelector('#btnAdd');
+let inputTask = document.querySelector('#addTask');
 
 document.addEventListener("DOMContentLoaded", async ()=>{
+    taskListIndex();
+})
+
+btnAdd.addEventListener('click', async () => {
+    if (inputTask.value.trim() !== "") {
+        await addTask({ task: inputTask.value.trim() });
+        inputTask.value = "";
+        taskListIndex();
+    }
+});
+
+const taskListIndex = async()=>{
     let res = await allTaskList();
     listTask.innerHTML = indexTask(res);
-})
+}
--- js/module/app.js
+++ js/module/app.js
@@ -9,3 +9,14 @@ export const getTask = async (id) => {
     let data = await res.json();
     return data;
 }
+export const addTask = async (task) => {
+    let res = await fetch(`https://6674179975872d0e0a950e53.mockapi.io/todoList`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    });
+    let data = await res.json();
+    return data;
+}
